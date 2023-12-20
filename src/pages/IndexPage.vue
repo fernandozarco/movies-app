e86548d2d8665a96472f16b8921b4ecf08418901
fernando_zarco <fernandozarcoconde@gmail.com>
2023-12-20 17:32:47 -0400

    Inicializacion del proyecto

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,102 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="q-my-">
+      <label for="">Busca por titulo</label>
+      <input type="text" v-model="query">
+      <button @click="searchMovies">Buscar</button>
+    </div>
+
+    <div class="q-pa-md row items-start q-gutter-md" style="width: 100%;">
+      <q-card class="my-card" v-for="movie in movies" :key="movie.imdbID">
+        <q-img :src="movie.Poster" @click="showMovie(movie.imdbID)">
+          <div class="absolute-bottom text-subtitle2 text-center">
+            {{ movie.Title }}
+          </div>
+        </q-img>
+      </q-card>
+    </div>
+
+    <q-dialog v-model="card">
+      <MoviePage :movieData="movie" />
+      <!-- <q-card class="my-card">
+        <q-img :src="movie.Poster" />
+
+        <q-card-section>
+          <div class="row no-wrap items-center">
+            <div class="col text-h6 ellipsis">
+              {{ movie.Title }}
+            </div>
+          </div>
+
+          <q-rating v-model="stars" :max="5" size="32px" />
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <div class="text-subtitle1">
+            {{ movie.Director }}
+          </div>
+          <div class="text-caption text-grey">
+            {{ movie.Actors }}
+          </div>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-actions align="right">
+          <q-btn v-close-popup flat color="primary" label="Add favorites" />
+        </q-card-actions>
+      </q-card> -->
+    </q-dialog>
+  </q-page>
+</template>
+
+<style scoped>
+.my-card {
+  width: 100%;
+  max-width: 250px;
+}
+</style>
+
+<script>
+import axios from 'axios'
+import { ref } from 'vue'
+import MoviePage from 'src/pages/MoviePage.vue'
+
+export default {
+  name: 'IndexPage',
+  components: {
+    MoviePage
+  },
+  setup() {
+    const movies = ref([])
+    const query = ref('')
+    const card = ref(false)
+    const movie = ref({})
+
+    async function searchMovies() {
+      console.log('search mvies', query.value)
+      const { data } = await axios.get(`https://www.omdbapi.com/?apikey=bad0d056&s=${query.value}`)
+      console.log('data', data)
+      movies.value = data.Search
+      console.log('movies dta', movies.value)
+    }
+
+    async function showMovie(movieId) {
+      console.log('showMovie(movie.imdbID)', movieId)
+      const { data } = await axios.get(`https://www.omdbapi.com/?apikey=bad0d056&i=${movieId}`)
+      console.log('movie', data)
+      movie.value = data
+      card.value = true
+    }
+
+    return {
+      movies,
+      movie,
+      query,
+      card,
+      searchMovies,
+      showMovie
+    }
+  }
+}
+</script>
---
+++ src/pages/MoviePage.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-card class="my-card">
+    <q-img :src="movieData.Poster" />
+
+    <q-card-section>
+      <div class="row no-wrap items-center">
+        <div class="col text-h6 ellipsis">
+          {{ movieData.Title }}
+        </div>
+      </div>
+
+      <q-rating v-model="stars" :max="5" size="32px" />
+    </q-card-section>
+
+    <q-card-section class="q-pt-none">
+      <div class="text-subtitle1">
+        {{ movieData.Director }}
+      </div>
+      <div class="text-caption text-grey">
+        {{ movieData.Actors }}
+      </div>
+    </q-card-section>
+
+    <q-separator />
+
+    <q-card-actions align="right">
+      <q-btn v-close-popup flat color="primary" label="Add favorites" />
+    </q-card-actions>
+  </q-card>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+export default {
+  name: 'MoviePage',
+  props: {
+    movieData: Object
+  },
+  setup(props) {
+
+    onMounted(() => {
+      console.log('shego al movie ', props.movieData)
+    })
+    return {}
+  }
+}
+</script>
+
+<style></style>
