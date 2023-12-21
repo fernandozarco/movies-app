6d400b4812a3bb31bf683dd2ea38e019fd3a7219
fernando_zarco <fernandozarcoconde@gmail.com>
2023-12-21 17:54:24 -0400

    Se agrego la vista de login

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="window-height window-width row justify-center centers items-center">
+    <div style="width: 400px;">
+      <q-form @submit="onSubmit">
+        <q-input filled v-model="username" label="Usuario *" lazy-rules
+          :rules="[val => val && val.length > 0 || 'Please type something']" />
+
+        <q-input filled type="password" v-model="password" label="Contraseña *" lazy-rules :rules="[
+          val => val !== null && val !== '' || 'Please type your password'
+        ]" />
+
+        <div>
+          <q-btn label="Iniciar sesión" type="submit" color="primary" />
+        </div>
+      </q-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'LoginPage',
+  setup() {
+    const username = ref(null)
+    const password = ref(null)
+
+    return {
+      username,
+      password,
+
+      onSubmit() {
+        console.log('logged in')
+      },
+    }
+  }
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,10 @@
 const routes = [
   {
     path: '/',
+    component: () => import('pages/LoginPage.vue'),
+  },
+  {
+    path: '/movies',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
